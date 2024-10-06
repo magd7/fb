f421a27bb65b51fe0326b55685ae93baaa0a6c40
magd <h8magd@gmail.com>
2024-10-06 15:15:56 +0300

    caa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio'); // For parsing HTML
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
